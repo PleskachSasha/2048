026a715e6e5dac0186e9d4395a5a47feaa79b8c4
Oleksandr <9pleskach9@gmail.com>
2022-05-21 10:49:33 +0300

    Right Left

--- 2048/2048.cpp
+++ 2048/2048.cpp
@@ -84,7 +84,6 @@ void MoveLeft()
 							break;
 						}
 						++itL;
-						
 					}
 
 
@@ -107,9 +106,7 @@ void MoveLeft()
 								rFlag = true;
 								break;
 							}
-							++itR;
-							//???????
-							
+							++itR;							
 						}
 					}
 
@@ -154,33 +151,6 @@ void MoveLeft()
 								next = 4;
 								break;
 							}
-							
-							
-
-							//BREAK????????
-
-							/*else if (board[i][itL] == board[i][itR] && count != 1)
-							{
-								if (board[i][j] == 0 && itL < j)
-								{
-									board[i][j] = board[i][itL] * 2;
-									board[i][itR] = 0;
-									board[i][itL] = 0;
-									break;
-								}
-								else
-								{
-									board[i][itL] *= 2;
-									board[i][itR] = 0;
-									break;
-								}
-							}
-							else if (board[i][itL] != board[i][itR] && count != 1 && board[i][j] == 0)
-							{
-								board[i][j] = board[i][itR];
-								board[i][itR] = 0;
-								break;
-							}*/
 						}
 					}
 					
@@ -191,13 +161,126 @@ void MoveLeft()
 	}
 }
 
+void MoveRight()
+{
+	for (int i = 3; i >= 0; --i)
+	{
+		for (int next = 3; next >= 0; --next)
+		{
+			bool zeroFlag = false;
+			bool endWhile = false;
 
-			
-		
+			while (!endWhile)
+			{
+				int itL = 4;
+				int itR = next;
 
-	
+				int count = 0;
 
-void MoveRight() {}
+				for (int q = 0; q < 4; q++)
+				{
+					if (board[i][q] != 0)
+					{
+						count++;
+					}
+				}
+				if (count == 0)
+				{
+					endWhile = true;
+				}
+
+				if (!endWhile)
+				{
+					bool rFlag = false;
+					bool lFlag = false;
+
+					while (!rFlag)
+					{
+						if (board[i][itR] != 0 || itR < 0)
+						{
+							rFlag = true;
+							break;
+						}
+						--itR;
+					}
+
+					
+
+
+					if (count > 1)
+					{
+						itL = 3;
+						while (!lFlag)
+						{
+							if (itL == itR)
+							{
+								--itL;
+							}
+							if (itL < 0)
+							{
+								itL = 4;
+								break;
+							}
+							if (board[i][itL] != 0 && itL < itR)
+							{
+								lFlag = true;
+								break;
+							}
+							--itL;
+						}
+					}
+
+					if (itR >= 0)
+					{
+						for (int j = 3; j >= 0; --j)
+						{
+
+							if (board[i][itL] == board[i][itR] && board[i][j] == 0 && j > itR)
+							{
+								board[i][j] = board[i][itR] * 2;
+								board[i][itR] = 0;
+								board[i][itL] = 0;
+								++j;
+								break;
+
+							}
+							else if (board[i][itL] == board[i][itR] && j < itR)
+							{
+								board[i][itR] *= 2;
+								board[i][itL] = 0;
+								++j;
+								break;
+							}
+							else if (board[i][itL] != board[i][itR] && count > 1 && itL < j && itR > j && board[i][j] == 0)
+							{
+								board[i][j] = board[i][itL];
+								board[i][itL] = 0;
+								break;
+							}
+							else if (board[i][itL] != board[i][itR] && count > 1 && itL < j && itR < j && board[i][j] == 0)
+							{
+								board[i][j] = board[i][itR];
+								board[i][itR] = board[i][itL];
+								board[i][itL] = 0;
+								break;
+							}
+							else if (board[i][j] == 0 && itR != 3 && j > itR)
+							{
+								board[i][j] = board[i][itR];
+								board[i][itR] = 0;
+								next = -1;
+								break;
+							}
+						}
+					}
+
+				}
+				endWhile = true;
+			}
+		}
+	}
+}
+
 void MoveDown() {}
 void MoveUp() {}
 
@@ -240,210 +323,8 @@ int main()
 		cin >> i;
 		if (i == 1)
 		{
-			MoveLeft();
-		}
-		
-		
-		
-		
+			MoveRight();
+		}		
 	}
-		
 }
 
-//void MoveLeftZero(int _itL, int _itR)
-//{
-//	for (int i = 0; i < 4; ++i)
-//	{
-//		for (int j = 0; j < 4; j++)
-//		{
-//			int count = 0;
-//			for (int k = 0; k < 4; k++)
-//			{
-//				if (board[i][k] != 0)
-//				{
-//					++count;
-//				}
-//			}
-//
-//			bool endWhile = false;
-//			int itL = _itL;
-//			int itR = _itR;
-//
-//			while (!endWhile)
-//			{
-//
-//				bool rFlag = false;
-//				bool lFlag = false;
-//
-//				while (!rFlag)
-//				{
-//					if (board[i][itR] != 0)
-//					{
-//						rFlag = true;
-//						break;
-//					}
-//					--itR;
-//					if (itR < 1)
-//					{
-//						rFlag = true;
-//					}
-//				}
-//				while (!lFlag)
-//				{
-//
-//					if (board[i][itL] != 0)
-//					{
-//						lFlag = true;
-//						break;
-//					}
-//					++itL;
-//					if (itL > 3)
-//					{
-//						lFlag = true;
-//					}
-//				}
-//
-//				if (count == 1 && itR == itL && board[i][0] == 0)
-//				{
-//					board[i][0] = board[i][itR];
-//					board[i][itR] = 0;
-//					break;
-//				}
-//				/*if (board[i][itL] == board[i][itR])
-//				{
-//					board[i][itL] *= 2;
-//					board[i][itR] = 0;
-//					break;
-//				}*/
-//
-//
-//
-//				endWhile = true;
-//			}
-//		}
-//
-//	}
-//}
-//
-//void MoveLeft()
-//{
-//	int itR = BOARD_SIZE - 1;
-//
-//	int itL = 1;
-//
-//	for (int i = 0; i < 4; i++)
-//	{
-//		MoveLeftZero(i, itR);
-//	}
-//
-//
-//
-//
-//
-//}
-
-//for (int i = 0; i < 4; i++)
-	//{
-	//	bool zeroFlag = false;
-
-	//	bool endWhile = false;
-	//	int itL = 0;
-	//	int itR = 3;
-
-	//	while (!endWhile)
-	//	{
-	//		int count = 0;
-	//		for (int q = 0; q < 4; q++)
-	//		{
-	//			if (board[i][q] != 0)
-	//			{
-	//				count++;
-	//			}
-	//		}
-
-	//		bool rFlag = false;
-	//		bool lFlag = false;
-
-	//		while (!rFlag)
-	//		{
-	//			if (board[i][itR] != 0)
-	//			{
-	//				rFlag = true;
-	//				break;
-	//			}
-	//			--itR;
-	//			if (itR < 1)
-	//			{
-	//				rFlag = true;
-	//			}
-	//		}
-
-	//		while (!lFlag && !(board[i][itL] == board[i][itR]))
-	//		{
-	//			if (board[i][itL] != 0)
-	//			{
-	//				lFlag = true;
-	//				break;
-	//			}
-	//			++itL;
-	//			if (itL > 3)
-	//			{
-	//				lFlag = true;
-	//			}
-	//		}
-
-	//		
-	//		for (int j = 0; j < 4; j++)
-	//		{
-	//			if (count == 1 && board[i][j] == 0 && itR>j)
-	//			{
-	//				board[i][j] = board[i][itR];
-	//				board[i][itR] = 0;
-	//				break;
-
-	//			}
-	//			else if (board[i][itL] == board[i][itR] && count != 1)
-	//				{
-	//					if (board[i][j] == 0 && itL < j)
-	//					{
-	//						board[i][j] = board[i][itL] * 2;
-	//						board[i][itR] = 0;
-	//						board[i][itL] = 0;
-	//					}
-	//					else {
-	//						board[i][itL] *= 2;
-	//						board[i][itR] = 0;
-	//					}
-	//				}
-	//			else
-	//			{
-	//				if (board[i][j] == 0 && count != 1 && itR < j)
-	//				{
-	//					board[i][j] = board[i][itR];
-	//					board[i][itR] = 0;
-	//					
-	//				}
-	//			}
-	//			else
-	//			{
-	//				if (board[i][j] == 0 && lflag)
-	//				{
-
-	//					break;
-	//				}
-	//			}
-	//			
-	//				
-	//			
-	//		}
-	//					
-	//		/*if (board[i][itL] == board[i][itR])
-	//		{
-	//			board[i][itL] *= 2;
-	//			board[i][itR] = 0;
-	//			break;
-	//		}*/
-	//		
-	//		endWhile = true;
-	//	}
-	//}
